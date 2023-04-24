8ff6f7822f573e94c26740e63a4ebba0ed66c2c2
Alan Haynes <aghaynes@users.noreply.github.com>
2023-04-24 15:39:28 +0200

    shiny options

--- R/run_costing_app.R
+++ R/run_costing_app.R
@@ -1,9 +1,9 @@
 # run app
 #' @export
 
-run_costing_app <- function(...){
+run_costing_app <- function(opts = list()){
   shiny::shinyAppDir(system.file(package = "CTUCosting",
                           "app"),
-                     ...)
+                     options = opts)
 }
 
