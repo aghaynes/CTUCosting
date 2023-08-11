992615310af8b4a60068f6af6e637f4896dc4950
Alan Haynes <aghaynes@users.noreply.github.com>
2023-08-11 16:19:04 +0200

    look up tables as CSVs

--- R/lookup_tables.R
+++ R/lookup_tables.R
@@ -8,40 +8,43 @@ rateopts <- tibble::tribble(
 4, "SNF",
 )
 
+# lookup table of service to service name to division and formname
+servicenames <- read.csv(system.file("data/servicenames.csv", package = "CTUCosting"))
+#   tibble::tribble(
+#   ~service, ~Service,                    ~div ,   ~form,
+#   "docdev", "Document development",      "MON",   "document_development",
+#   "dml",    "Data management (light)",   "DM",    "dm_redcap_light",
+#   "dmf",    "Data management (full)",    "DM",    "dm_full_services",
+#   "sta",    "Statistics",                "STA",   "statistics",
+#   "csm",    "Clinical study management", "CSM",   "clinical_study_management",
+#   "qm",     "Quality management",        "QM",    "quality_management",
+#   "ci",     "Clinical investigation",    "CI",    NA,
+#   "admin",  "Administration",            "ADMIN", NA,
+#   "sen",    "Seniors",                   "SEN",   NA,
+#   "rs",     "Regulatory services",       "MON",   "regulatory_support",
+#   "sw",     "Study website",             "DM",    "study_website",
+#   "cloud",  "ShareFile cloud",           "DM",    "sharefile_cloud",
+#   "mon",    "Monitoring",                "MON",   "monitoring_onsite_remote",
+#   "cdm",    "Central data monitoring",   "MON",   "central_data_monitoring",
+#   "cdmra",  "Central data monitoring",   "MON",   "central_data_monitoring",
+#   "ra",     "Research assistant",        NA,      NA
+# )
 
-servicenames <- tibble::tribble(
-  ~service, ~Service,                    ~div ,   ~form,
-  "docdev", "Document development",      "MON",   "document_development",
-  "dml",    "Data management (light)",   "DM",    "dm_redcap_light",
-  "dmf",    "Data management (full)",    "DM",    "dm_full_services",
-  "sta",    "Statistics",                "STA",   "statistics",
-  "csm",    "Clinical study management", "CSM",   "clinical_study_management",
-  "qm",     "Quality management",        "QM",    "quality_management",
-  "ci",     "Clinical investigation",    "CI",    NA,
-  "admin",  "Administration",            "ADMIN", NA,
-  "sen",    "Seniors",                   "SEN",   NA,
-  "rs",     "Regulatory services",       "MON",   "regulatory_support",
-  "sw",     "Study website",             "DM",    "study_website",
-  "cloud",  "ShareFile cloud",           "DM",    "sharefile_cloud",
-  "mon",    "Monitoring",                "MON",   "monitoring_onsite_remote",
-  "cdm",    "Central data monitoring",   "MON",   "central_data_monitoring",
-  "cdmra",  "Central data monitoring",   "MON",   "central_data_monitoring",
-  "ra",     "Research assistant",        NA,      NA
-)
+# lookup table of division abbreviation and full name
+divnames <- read.csv(system.file("data/divisionnames.csv", package = "CTUCosting"))
+#   tibble::tribble(
+#   ~div, ~division,
+#   "ADMIN", "Administration",
+#   "CI", "Clinical Investigation",
+#   "CSM", "Clinical Study Management",
+#   "DM", "Data Management",
+#   "MON", "Monitoring",
+#   "QM", "Quality Management",
+#   "SEN", "Seniours",
+#   "STA", "Statistics",
+# )
 
-divnames <- tibble::tribble(
-  ~div, ~division,
-  "ADMIN", "Administration",
-  "CI", "Clinical Investigation",
-  "CSM", "Clinical Study Management",
-  "DM", "Data Management",
-  "MON", "Monitoring",
-  "QM", "Quality Management",
-  "SEN", "Seniours",
-  "STA", "Statistics",
-)
-
-
+# lookup table of variable prefixes and applicable rates
 ratenames <- tibble::tribble(
   ~service, ~rate_name,
   "docdev", "rate_mon",
@@ -63,3 +66,6 @@ ratenames <- tibble::tribble(
   "cdmra",  "rate_ra"
 
 )
+
+# clinic/institute heads
+clinic_heads <- read.csv(system.file("data/clinic_heads.csv", package = "CTUCosting"))
