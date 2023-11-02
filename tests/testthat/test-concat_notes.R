c71806fab8952c70081c221f02d2edd75e688e28
Alan Haynes <aghaynes@users.noreply.github.com>
2023-11-02 14:03:52 +0100

    tests for get_notes and concat_notes

---
+++ tests/testthat/test-concat_notes.R
@@ -0,0 +1,24 @@
+test_that("get_notes extracts correct variables", {
+
+  dfs <- list(
+    statistics = tibble::tribble(
+      ~foo, ~bar, ~foobar_notes,
+      "a", "b", "c"
+    ),
+    dm_redcap_light = tibble::tribble(
+      ~flibble, ~fribble, ~dml_notes_standard,
+      "aa", "bb", "cc"
+    ),
+    dm_full_services = tibble::tribble(
+      ~flobble, ~frobble, ~dmf_notes_2, ~dmf_notes,
+      "aa", "bb", "ccc", "ddd"
+    )
+  )
+
+  x <- get_notes(dfs)
+  cx <- concat_notes(x)
+  expect_equal(cx, "**Statistics**\n\nc\n\n**Data management (light)**\n\ncc\n\n**Data management (full)**\n\nddd\n\nccc")
+  expect_equal(length(cx), 1)
+
+
+})
