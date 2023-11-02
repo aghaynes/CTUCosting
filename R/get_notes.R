98e9510531b2d9cb55a14a925e3eef432090457a
Alan Haynes <aghaynes@users.noreply.github.com>
2023-11-02 14:15:40 +0100

    better handling of pages without notes

--- R/get_notes.R
+++ R/get_notes.R
@@ -4,7 +4,9 @@
 #' @export
 get_notes <- function(data){
   vals <- lapply(data, function(x){
-    tmp <- x |> select(any_of("dml_notes_standard"), ends_with("notes"), any_of("dmf_notes_2"))
+    tmp <- x |>
+      select(any_of("dml_notes_standard"), ends_with("notes"), any_of("dmf_notes_2"))
+    if(nrow(tmp) > 0) tmp <- tmp |> select(where(~any(!is.na(.x))))
     if(ncol(tmp) > 1) tmp <- paste(tmp, collapse = "\n\n")
     unlist(tmp)
   })
