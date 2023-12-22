0676a31becc7b5125ab5144922865ecba60b4cc3
Alan Haynes <aghaynes@users.noreply.github.com>
2023-12-22 08:49:59 +0100

    use fixed REDCap version number

--- R/create_rc_link.R
+++ R/create_rc_link.R
@@ -8,13 +8,13 @@
 create_rc_link <- function(record, costing, token){
 
   event <- event_ids()[costing]
-  version <- redcap_export_tbl(token,
-                               "https://redcap.ctu.unibe.ch/api/",
-                               "version") |>
-                     names() |>
-    (function(x)gsub("^X", "", x))()
+  # version <- redcap_export_tbl(token,
+  #                              "https://redcap.ctu.unibe.ch/api/",
+  #                              "version") |>
+  #                    names() |>
+  #   (function(x)gsub("^X", "", x))()
 
-  paste0("https://redcap.ctu.unibe.ch/redcap_v", version,
+  paste0("https://redcap.ctu.unibe.ch/redcap_v", redcap_version(),
          "/DataEntry/index.php?pid=1132&id=", record,
          "&event_id=", event, "&page=meta_information")
 
