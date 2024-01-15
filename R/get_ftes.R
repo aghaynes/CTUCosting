2fad92d3421d83302493914dfe9b34997ad5c8e4
Alan Haynes <aghaynes@users.noreply.github.com>
2024-01-15 10:51:01 +0100

    add get_ftes

---
+++ R/get_ftes.R
@@ -0,0 +1,44 @@
+#' Extract FTE data from downloaded data
+#'
+#' @importFrom dplyr join_by
+#' @export
+
+get_ftes <- function(d, meta){
+
+  long <- d$full_time_equivalent |>
+    mutate(across(everything(), as.character)) |>
+    # select(-c(record_id, redcap_event_name, redcap_repeat_instrument, redcap_repeat_instance),
+    #        -matches("date|total_hours|cost|notes|author|complete")) %>%
+    # select(matches("_[[:digit:]]{1,2}")) |> names()
+    select(matches("(desc|prop|units|cost)_")) |>
+    pivot_longer(matches("(desc|prop|units|cost)_"),
+                 names_sep = "_", names_to = c("service", "var", "item")) |> #View()
+    pivot_wider(names_from = var) |> #View()
+    # select(fte_div, fte_) |>
+    mutate(across(c(prop, units, cost), as.numeric)) |>
+    select(desc, prop, units, cost)
+
+  if(nrow(long) > 0){
+    long <- long |>
+      filter(!is.na(desc) & desc != "")
+  }
+
+  print("FTEs long:")
+  print(long)
+
+  div <- specific_option(meta$metadata, d$full_time_equivalent, "fte_div")
+  role <- d$full_time_equivalent$fte_role
+  fte <- nrow(long) > 0
+
+  print(paste("FTE:", fte))
+
+  return(
+    list(
+      fte = fte,
+      costs = long,
+      division = div,
+      role = role
+    ))
+
+}
+
