b0658dd429a95a98e98b5ecb41e0ae8c6f4e2956
Alan Haynes <aghaynes@users.noreply.github.com>
2024-02-01 16:28:40 +0100

    more selector functions

--- R/select.R
+++ R/select.R
@@ -1,6 +1,7 @@
 # select variables for specific purposes
 # for admin - needs more info to set up PF
 #' @importFrom dplyr select
+#' @export
 select_for_admin <- function(data){
   Service <- wp <- wp_lab <- Hours <- Rate <- Cost <- NULL
   data |>
@@ -9,9 +10,36 @@ select_for_admin <- function(data){
 
 # for pdf
 #' @importFrom dplyr select ungroup
+#' @export
 select_for_pdf <- function(data){
   Service <- wp_lab <- Description <- Hours <- Cost <- NULL
   data |>
     ungroup() |>
     select(Service, Task = wp_lab, Description, Hours, Cost)
+}
+
+
+# expenses for pdf
+#' @importFrom dplyr select ungroup
+#' @export
+select_expenses_for_pdf <- function(data){
+  if(nrow(data) > 0){
+    out <- data |>
+      ungroup() |>
+      select(Division, Description, Amount) |>
+      rename(`Cost (CHF)` = Amount)
+  } else {
+    out <- NULL
+  }
+  return(out)
+}
+
+# expenses for admin
+#' @importFrom dplyr select ungroup
+#' @export
+select_expenses_for_admin <- function(data){
+    out <- data |>
+      ungroup() |>
+      select(wp_number = exp_pf, wp_lab, author = exp_author, Division, Description, Amount)
+  return(out)
 }
